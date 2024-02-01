27adc6e2d7790e2cc4e0967d09a414fca0808e8b
Monkey Postulate <cuatrooctavos@yahoo.com.mx>
2024-02-01 14:00:03 +0100

    Create hello_cbern.py
    Playground example

---
+++ playground/hello_cbern.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("Welcome to the cbern python library!")
+    st.write("This library is meant for document embeddings")
+
+if __name__ == "__main__":
+    main()
