b64efc08519523de10ce6276fb9bc3eab6df0906
Monkey Postulate <cuatrooctavos@yahoo.com.mx>
2024-02-01 14:42:07 +0100

    Update hello_cbern.py

--- playground/hello_cbern.py
+++ playground/hello_cbern.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 # Define a route for the root URL
 @app.route('/')
 def cbern_welcome_page():
-    return "<h1> Welcome to cbern!</h1> <p> This is a great search ecosystem!</p>"
+    return """<h1 style = "text-align:center"> Welcome to cbern!</h1>
+    <p> This is a great search ecosystem!</p>
+    <p>We are planning to have vector search for patent and swiss parlament document</p>
+    """
 
 # Run the Flask application if the script is executed
 if __name__ == '__main__':
